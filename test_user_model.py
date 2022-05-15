8c7916794801914f033dced91420fd9959d09670
Tony Alvarez <tonyalva87@gmail.com>
2022-05-15 15:34:03 -0700

    updated tests for user views and model

--- test_user_model.py
+++ test_user_model.py
@@ -4,7 +4,6 @@
 #
 #    python -m unittest test_user_model.py
 
-
 import os
 from unittest import TestCase
 
@@ -15,7 +14,7 @@ from models import db, User, Message, Follows
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test_db"
 
 
 # Now we can import app
@@ -26,11 +25,12 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
+db.drop_all()
 db.create_all()
 
 
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
+    """Test models for users."""
 
     def setUp(self):
         """Create test client, add sample data."""
@@ -40,6 +40,12 @@ class UserModelTestCase(TestCase):
         Follows.query.delete()
 
         self.client = app.test_client()
+        
+    def tearDown(self):
+        """Remove any temp data in session.
+        """
+        
+        db.session.rollback()
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -56,3 +62,31 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+    def test_user_details(self):
+        """
+        Test that user details can  be added to an
+        existing user.
+        """
+
+        test_user = User(
+            email="test@test.com",
+            username="testuser",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(test_user)
+        
+        test_user.image_url = "/test/imageUrl.jpg"
+        test_user.header_image_url = "/test/imageUrl.jpg"
+        test_user.location = "user location"
+        test_user.bio = "bio for the user"
+
+        db.session.commit()
+        
+        # User should have no messages & no followers
+        self.assertIn("/test/imageUrl.jpg", test_user.image_url)
+        self.assertIn("/test/imageUrl.jpg", test_user.header_image_url)
+        self.assertIn("user location", test_user.location)
+        self.assertIn("bio for the user", test_user.bio)
+
